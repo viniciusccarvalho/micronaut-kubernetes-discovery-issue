39066f87a33807b0cf49ed3572db2782a7695b3a
Vinicius Carvalho <viniciusccarvalho@gmail.com>
2020-09-11 14:06:19 -0400

    Fix with @ExecuteOn

--- frontend/src/main/java/com/example/BackendController.java
+++ frontend/src/main/java/com/example/BackendController.java
@@ -2,11 +2,17 @@ package com.example;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.scheduling.TaskExecutors;
+import io.micronaut.scheduling.annotation.ExecuteOn;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller("/backend")
+@ExecuteOn(TaskExecutors.IO)
 public class BackendController {
 
     private final BackendClient backendClient;
+    private final Logger logger = LoggerFactory.getLogger(BackendController.class);
 
     public BackendController(BackendClient backendClient) {
         this.backendClient = backendClient;
@@ -14,6 +20,11 @@ public class BackendController {
 
     @Get("/health")
     public String health() {
+        logger.info("Calling backend service");
         return backendClient.health();
     }
+
+    @Get("/hi")
+    public String hi() {return "hello";}
+
 }
